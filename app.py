3b8b0ea2347f98c345003a3210ed2ab6e0cc8161
Joshua Bell <josh.marc.bell@hotmail.com>
2023-04-04 19:17:13 +0800

    Data cleaning added, Flask app for API, SQL table schema

---
+++ app.py
@@ -0,0 +1,42 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/Project_3_db")
+
+Base = automap_base()
+
+Base.prepare(autoload_with=engine)
+
+Commodities = Base.classes.commodity_prices
+
+app = Flask(__name__)
+
+@app.route('/commodity_prices')
+def commodity_prices():
+    with engine.connect() as conn:
+        result = conn.execute('SELECT * From commodity_prices')
+        headers = result.keys()
+        rows = result.fetchall()
+        data = []
+        for row in rows:
+            data.append(dict(zip(headers, row)))
+        return jsonify(data)
+
+@app.route('/expendature')
+def expendature_by_commodity():
+    with engine.connect() as conn:
+        result = conn.execute('SELECT * From expendature_by_commodity')
+        headers = result.keys()
+        rows = result.fetchall()
+        data = []
+        for row in rows:
+            data.append(dict(zip(headers, row)))
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
